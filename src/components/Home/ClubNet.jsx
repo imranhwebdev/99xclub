cce209a701bd831325d5a305b324064cccf74492
imranhwp <imranh.wp@gmail.com>
2023-01-17 10:17:29 +0600

    clubnetupdate

--- src/components/Home/ClubNet.jsx
+++ src/components/Home/ClubNet.jsx
@@ -6,7 +6,7 @@ const ClubNet = () => {
   return (
     <section className="club__net__bg  relative">
       <div
-        className="absolute top-0 left-0 w-full h-full"
+        className="club_net_shap absolute top-0 left-0 w-full h-full"
         style={{
           backgroundImage: `url(${bg3})`,
           backgroundRepeat: "no-repeat",
@@ -14,14 +14,14 @@ const ClubNet = () => {
           backgroundPosition: "left center",
         }}
       ></div>
-      <div className="max-w-[1187px] lg:h-[455px] mx-auto py-4 px-4 md:px-0 flex justify-center items-center ">
+      <div className="max-w-[1187px] mx-auto px-4 flex justify-center items-center ">
         <div className="grid grid-cols-9 justify-center items-center">
           <div className="col-span-9 lg:col-span-4  ">
-            <div className="w-full md:mt-[-23%] lg:-mt-[52%] z-20 relative">
-              <img src={img} alt="" className="w-3/4" />
+            <div className="w-full md:mt-[-16%] lg:-mt-[40%] z-20 relative">
+              <img src={img} alt="" className="club_net_img" />
             </div>
           </div>
-          <div className="col-span-9 lg:col-span-5 sm:mt-0 my-6">
+          <div className="col-span-9 lg:col-span-5 sm:mt-0 club_net_content">
             <p className="text-[#E1E2EE] text-[24px] leading-[33px] md:text-[31px] md:leading-[44px] font-bold p-2">
               Owning one of these limited digital collectibles, and the image it
               represents, says to the world who you are, what you’re all about,
